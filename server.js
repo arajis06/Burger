0cffe65a6ac52880b5ff01d4eaf5ed5e0eeef89d
ALEXIS RAJIS <arajis06@gmail.com>
2019-07-30 12:41:28 -0400

    fixed password

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var mysql = require("mysql");
 var exphbs = require("express-handlebars");
 
 var app = express();
@@ -8,6 +7,8 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+//Serve static content for the app from "public" directory
+app.use(express.static(__dirname + "/public"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,19 +16,13 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "burgers_db"
-});
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+app.use(routes);
 
-  console.log("connected as id " + connection.threadId);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
